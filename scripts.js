61c05931f0a0061451b778e94cc22d07c27a3766
Rashsmit <33126899+Rashsmit@users.noreply.github.com>
2019-04-18 22:51:26 -0400

    grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 Chart.defaults.global.legend.display = false;
 var line = document.getElementById('myChart-1').getContext('2d');
 var bar = document.getElementById('myChart-2').getContext('2d');
+var pie = document.getElementById('myChart-3').getContext('2d');
 var lineChart = new Chart(line, {
     // The type of chart we want to create
     type: 'line',
@@ -9,6 +10,8 @@ var lineChart = new Chart(line, {
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
+          responsive: true,
+    maintainAspectRatio: true,
             label: 'My First dataset',
             backgroundColor: 'rgba(139, 142, 191, 0.35)',
             pointBackgroundColor: "white",
@@ -29,6 +32,8 @@ var barChart = new Chart(bar, {
 
     // The data for our dataset
     data: {
+      responsive: true,
+    maintainAspectRatio: true,
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
@@ -45,24 +50,26 @@ var barChart = new Chart(bar, {
     options: {}
 });
 
-// var chart = new Chart(ctx, {
-//     // The type of chart we want to create
-//     type: 'line',
-//
-//     // The data for our dataset
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgba(139, 142, 191, 0.35)',
-//             pointBackgroundColor: "white",
-//             pointstyle:'circle',
-//             borderColor: '#8b8ebf',
-//             radius: 6,
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     },
-//
-//     // Configuration options go here
-//     options: {}
-// });
+var pieChart = new Chart(pie, {
+    // The type of chart we want to create
+    type: 'pie',
+
+    // The data for our dataset
+    data: {
+      responsive: true,
+    maintainAspectRatio: true,
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgba(139, 142, 191, 0.35)',
+            pointBackgroundColor: "white",
+            pointstyle:'circle',
+            borderColor: '#8b8ebf',
+            radius: 6,
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
